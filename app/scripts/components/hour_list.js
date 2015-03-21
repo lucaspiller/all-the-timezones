f537373c6286b702432fde707337738222783144
Luca Spiller <luca@stackednotion.com>
2015-03-21 14:22:23 +0400

    Dynamically resize hour-list to fit page

--- app/scripts/components/hour_list.js
+++ app/scripts/components/hour_list.js
@@ -3,21 +3,39 @@ import moment from 'moment';
 import Hour   from './hour';
 
 export default React.createClass({
+  HOUR_WIDTH: 60,
+
   getDefaultProps(): any {
     return {
       time: moment()
     }
   },
 
+  calculateWidths(): any {
+    var minuteOffset    = 60 - this.props.time.minute();
+    this.marginLeft     = -this.HOUR_WIDTH + minuteOffset;
+    this.width          = window.innerWidth + (this.HOUR_WIDTH * 2);
+    this.hoursToDisplay = Math.ceil(this.width / this.HOUR_WIDTH);
+  },
+
   getStyle(): any {
-    let offset = 60 - this.props.time.minute();
-    return { marginLeft: offset + "px" };
+    this.calculateWidths();
+    return {
+      width: this.width + "px"
+    };
+  },
+
+  getInnerStyle(): any {
+    this.calculateWidths();
+    return {
+      marginLeft: this.marginLeft + "px",
+    };
   },
 
   getHours(): any {
     let baseTime = moment(this.props.time);
 
-    return Array.apply(0, Array(24)).map(() => {
+    return Array.apply(0, Array(this.hoursToDisplay)).map(() => {
       let result = moment(baseTime);
       baseTime.add(1, 'hour');
       return result;
@@ -26,9 +44,11 @@ export default React.createClass({
 
   render(): any {
     return <div className="hour-list" style={this.getStyle()}>
-      {this.getHours().map(function(result) {
-        return <Hour time={result} key={result.hour()} />;
-      })}
+      <div className="hour-list-inner" style={this.getInnerStyle()}>
+        {this.getHours().map(function(result) {
+          return <Hour time={result} key={result.day() + "-" + result.hour()} />;
+        })}
+      </div>
     </div>;
   }
 });
